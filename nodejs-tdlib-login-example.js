8c17d8275eebf65013204686e0bca422c4e9dad5
atomdeniz <38922410+atomdeniz@users.noreply.github.com>
2019-05-21 12:21:00 +0300

    Update nodejs-tdlib-login-example.js

--- nodejs-tdlib-login-example.js
+++ nodejs-tdlib-login-example.js
@@ -8,7 +8,7 @@
 
 const Client  = require('tdl')
 const TDLib  = require('tdl-tdlib-ffi')
-const {API_ID, API_HASH, BOT_TOKEN} = require('./config.js');
+const API_ID, API_HASH, BOT_TOKEN = require('./config.js');
 
 const myDebug = false; // if you want to see logs change this to true
 const showSommeryLogs = true;
