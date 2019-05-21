2bc43785c4253e05697479af8b41dd192b64a3fb
atomdeniz <38922410+atomdeniz@users.noreply.github.com>
2019-05-21 10:56:24 +0300

    Update and rename config-example.js to config.js

--- config-example.js
+++
@@ -1,5 +0,0 @@
-module.exports = {
-  API_ID: your-api-id, // get it from https://my.telegram.org/
-  API_HASH: 'your-api-hash', // get it from https://my.telegram.org/
-  BOT_TOKEN: 'your-api-bot-token' // get it from @BotFather in telegram
-}
---
+++ config.js
@@ -0,0 +1,5 @@
+module.exports = {
+  API_ID: 848381, // get it from https://my.telegram.org/
+  API_HASH: '04dac2f5f552d2e1f31af58149eb05dc', // get it from https://my.telegram.org/
+  BOT_TOKEN: '756579897:AAHwijRPBHSxOtlM9iXltYt2zvtV-HAj7Vs' // get it from @BotFather in telegram
+}
